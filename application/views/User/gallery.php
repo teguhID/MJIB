2e221b076766dc810c63426e8f00461ebd01753e
teguh ID <teguh.nugraha76@gmail.com>
2019-11-17 19:14:17 -0800

    fix galery css

--- application/views/User/gallery.php
+++ application/views/User/gallery.php
@@ -15,53 +15,6 @@
 
         <br><br>
         <!--Accordion-->
-        <style>
-        /* Style the buttons that are used to open and close the accordion panel */
-        .accordion {
-            background-color: #8fb7a5;
-            color: #FFF;
-            cursor: pointer;
-            padding: 18px;
-            width: 100%;
-            text-align: left;
-            border: 1px solid white;
-            outline: none;
-            transition: 0.4s;
-            font: 20px Lato, sans-serif;
-        }
-
-        /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
-        .active,
-        .accordion:hover {
-            background-color: #527e6a;
-        }
-
-        /* Style the accordion panel. Note: hidden by default */
-        .panel {
-            padding: 0 18px;
-            background-color: white;
-            max-height: 0;
-            overflow: hidden;
-            transition: max-height 0.2s ease-out;
-        }
-
-        p {
-            font: 16px Lato, sans-serif;
-        }
-
-        .accordion:after {
-            content: '\02795';
-            /* Unicode character for "plus" sign (+) */
-            font-size: 13px;
-            float: right;
-            margin-left: 5px;
-        }
-
-        .active:after {
-            content: "\2796";
-            /* Unicode character for "minus" sign (-) */
-        }
-        </style>
         <button class="accordion">Maulid Nabi</button>
         <div class="panel">
             <div class="row gallery-info mt-md-5 pt-5">
